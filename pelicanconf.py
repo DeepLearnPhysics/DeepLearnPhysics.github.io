935cee3ca7dda573e04c8d4123f97129eddcca3b
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-12-06 16:45:46 -0800

    added a blog link, updated email

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'drinkingkazu'
-SITENAME = u'Scratch Blog'
+SITENAME = u'DeepLearnPhysics'
 SITEURL = 'https://deeplearnphysics.org'
 #SITEURL = '' #uncomment for local development
 PATH = 'content'
@@ -28,17 +28,6 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
